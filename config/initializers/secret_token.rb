478ad28519395eb3cd5599bafb5f619b39654b00
Kirill Strelkov <kirill.strelkov@gmail.com>
2014-10-21 14:51:43 -0300

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodlaptop::Application.config.secret_token = '6622a92f62bcea27c7e7e0d0e05d65653ee84b06a623032501dedb17aaba60cae69b9b04c6c3e92d434b7260208234d91577035d7613862ffa55dc5b05d4ea15'
