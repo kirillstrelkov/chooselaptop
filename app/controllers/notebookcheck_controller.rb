ffcf4e04103e5aada0d89817c854baf743c76130
Kirill Strelkov <kirill.strelkov@gmail.com>
2014-10-26 15:08:40 -0300

    fixed cpus/name.json url

--- app/controllers/notebookcheck_controller.rb
+++ app/controllers/notebookcheck_controller.rb
@@ -18,7 +18,7 @@ class NotebookcheckController < ApplicationController
   
   def get_cpu
     cpu_name = params[:name]
-    cpu_data = get_cpu_data cpu_name_from_notebookcheck
+    cpu_data = get_cpu_data_from_notebookcheck cpu_name
     respond_to do |format|
       format.json {render :json => cpu_data}
     end
@@ -26,7 +26,7 @@ class NotebookcheckController < ApplicationController
   
   def get_gpu
     gpu_name = params[:name]
-    gpu_data = get_gpu_data gpu_name_from_notebookcheck
+    gpu_data = get_gpu_data_from_notebookcheck gpu_name
     respond_to do |format|
       format.json {render :json => gpu_data}
     end
